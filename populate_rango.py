53bcf3d42ca9adbc210e7b14b643821d1895e718
Tariq Essa <2289124e@student.gla.ac.uk>
2018-01-26 18:22:59 +0000

    Chap6

--- populate_rango.py
+++ populate_rango.py
@@ -10,27 +10,40 @@ def populate():
 
     python_pages =[
         {"title":"Official Python Tutorial",
-         "url":"http://docs.python.org/2/tutorial/"},
+         "url":"http://docs.python.org/2/tutorial/",
+         "veiws": 11 },
+
         {"title":"How to Think like a Computer Scientist",
-         "url":"http://www.greenteapress.com/thinkpython/"},
+         "url":"http://www.greenteapress.com/thinkpython/",
+         "veiws": 12},
+
         {"title":"Learn Python in 10 Minutes",
-         "url":"http://www.korokithakis.net/tutorials/python/"}
+         "url":"http://www.korokithakis.net/tutorials/python/",
+         "veiws": 13}
         ]
 
     django_pages =[
         {"title":"Official Django Tutorial",
-         "url":"http://docs.djangoproject.com/en/1.9/intro/tutorial01/"},
+         "url":"http://docs.djangoproject.com/en/1.9/intro/tutorial01/",
+         "veiws": 14},
+
         {"title":"Django Rocks",
-         "url":"http://www.djangorocks.com/"},
+         "url":"http://www.djangorocks.com/",
+         "veiws": 15},
+
         {"title":"How to Tango with Django",
-         "url":"http://www.tangowithdjango.com/"}
+         "url":"http://www.tangowithdjango.com/",
+         "veiws": 16}
         ]
 
     other_pages =[
         {"title":"Bottle",
-         "url":"http://bottlepy.org/docs/dev/"},
+         "url":"http://bottlepy.org/docs/dev/",
+         "veiws": 17},
+
         {"title":"Flask",
-         "url":"http://flask.pocoo.org"}
+         "url":"http://flask.pocoo.org",
+         "veiws": 9000}
         ]
 
     cats = {"Python":{"pages": python_pages,"views":128, "likes":64},
@@ -47,7 +60,7 @@ def populate():
         for p in Page.objects.filter(category=c):
             print("- {0} - {1}".format(str(c), str(p)))
 
-def add_page(cat, title, url, views=0):
+def add_page(cat, title, url, views):
     p = Page.objects.get_or_create(category=cat, title=title)[0]
     p.url = url
     p.views = views
